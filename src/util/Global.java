e77bf5a5d5ed5f5a24aa08946211f907bedd4bd8
gmf <mg3534@columbia.edu>
2015-05-17 15:16:55 -0400

    add basic classes

---
+++ src/util/Global.java
@@ -0,0 +1,12 @@
+package util;
+
+public final class Global {
+	
+	public static String USERID;
+	public static String USERNAME;
+	public static String PATH;
+	public static boolean BLOCK = false;
+
+	
+	
+}
