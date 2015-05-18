c385a426046646fe28effbf7b6fa1ee30b5a4eb7
gmf <mg3534@columbia.edu>
2015-05-18 02:45:50 -0400

    modify normal

--- src/data/Normal.java
+++ src/data/Normal.java
@@ -1,9 +1,11 @@
 package data;
 
 public class Normal extends EventMSG{
-	public Normal(String id, String text, String lat, String lng, String timestamp) {
+	public Normal(String id, String text, String lat, String lng, 
+			String timestamp, String topic) {
 		super( id,  text,  lat,  lng,  timestamp);
 		// TODO Auto-generated constructor stub
+		this.topic = topic;
 	}
 	public String topic;
 	public String total_likes;
