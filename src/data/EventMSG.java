e7c5defef14c0bc40b4c3a0b7df7b31e6a982e48
fy <305120283@qq.com>
2015-05-18 02:42:28 -0400

    constructor

--- src/data/EventMSG.java
+++ src/data/EventMSG.java
@@ -5,4 +5,12 @@ public class EventMSG {
 	public String text;
 	public String lat, lng;
 	public String timestamp;
+	
+	public EventMSG(String id, String text, String lat, String lng, String timestamp){
+		this.id = id;
+		this.text = text;
+		this.lat = lat;
+		this.lng = lng;
+		this.timestamp = timestamp;
+	}
 }
